b0d074e1080226e48e65b755af7173db5c9969e2
nay <kunvunk@gmail.com>
2018-07-18 17:07:57 +0800

    convert surprise to class

--- surprise_github.py
+++ surprise_github.py
@@ -7,33 +7,38 @@ import math
 import os
 import sys
 
+class SurpriseRecommend:
+
+    def __init__(self):
+        self.algo = None
+
+    def fit(self,df_source: pd.DataFrame):
+        '''
+        df_source: 数据类型是pandas DataFrame，里面的数据结构是 user/item/weight
+        '''
+        reader = Reader(rating_scale=(0,1))
+        data = Dataset.load_from_df(df_source,reader=reader)
+        sim_options = {'name':'cosine','user_based':False}
+        self.algo = KNNBaseline(sim_options=sim_options)
+        trainset = data.build_full_trainset()
+        self.algo.fit(trainset)
+
+    def get_similar_items(self,item_id, count: int=10):
+        inner_item_id = self.algo.trainset.to_inner_iid(item_id)
+        sim_item_inner_ids = self.algo.get_neighbors(inner_item_id,count)
+        return (self.algo.trainset.to_raw_iid(inner_id) for inner_id in sim_item_inner_ids)
+
+model = SurpriseRecommend()
+
 file_path = os.path.expanduser(sys.argv[1])
 # file_path = "~/Downloads/sw_order_product.csv"
 df = pd.read_csv(file_path,names=["user","item","weight"])
+model.fit(df)
 
-reader = Reader(rating_scale=(0,1))
-data = Dataset.load_from_df(df,reader=reader)
-
-sim_options = {'name':'cosine','user_based':False}
-algo = KNNBaseline(sim_options=sim_options)
-
-#cross_validate(algo,data,measures=['RMSE','MAE'],cv=5,verbose=True)
-
-trainset = data.build_full_trainset()
-algo.fit(trainset)
-
-items = df.groupby("item")
-
+items = df.groupby('item')
 output_filename = os.path.splitext(sys.argv[0])[0]+".csv"
 with open(output_filename,"w") as f: 
-    for item,_group in items:
-        item_iid = algo.trainset.to_inner_iid(item)
-        neighbor_iids = algo.get_neighbors(item_iid,k=10)
-        neighbor_item_ids = (algo.trainset.to_raw_iid(inner_id) for inner_id in neighbor_iids)
+    for item,_ in items:
         f.write("%s,%s,%s\n" % (item,item,'NaN'))
-        for inner_id in neighbor_iids:
-            raw_id = algo.trainset.to_raw_iid(inner_id)
-            f.write("%s,%s,%s\n" % (item,raw_id,'NaN'))
-        # for raw_id in neighbor_item_ids:
-        #     f.write("%s,%s,%s\n" % (item,raw_id,'NaN'))
-
+        for sim_item_id in model.get_similar_items(item):
+            f.write("%s,%s,%s\n" % (item,sim_item_id,'NaN'))
