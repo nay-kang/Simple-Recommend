48ee761b8a07dec213921ab73ed5c2077789cc2e
nay <kunvunk@gmail.com>
2018-07-27 18:33:52 +0800

    change mysql drive to stock

--- data_source.py
+++ data_source.py
@@ -1,8 +1,10 @@
-import pymysql.cursors
+# import pymysql.cursors
+from mysql import connector
 import pandas as pd
+from datetime import datetime
 
 def _get_db_connection(conf):
-    connection = pymysql.connect(host=conf['db_host'],
+    connection = connector.connect(host=conf['db_host'],
                                 user=conf['db_user'],
                                 password=conf['db_password'],
                                 db=conf['db_database'],
@@ -10,22 +12,22 @@ def _get_db_connection(conf):
     return connection
 
 def read_from_database(conf):
-    connection = _get_db_connection(conf)
+    connection = _get_db_connection(conf)  
     df = pd.read_sql(conf['fetch_sql'],connection)
     return df
 
 def write_to_database(conf,data):
     connection = _get_db_connection(conf)
-    with connection.cursor() as cursor:
-        for item_id,sim in data.items():
-            if conf['mode'] == 'row':
-                for sim_item,weight in sim:
-                    cursor.execute(conf['insert_sql'] % (item_id,sim_item))
+    cursor = connection.cursor()
+    for item_id,sim in data.items():
+        if conf['mode'] == 'row':
+            for sim_item,weight in sim:
+                cursor.execute(conf['insert_sql'] % (item_id,sim_item))
 
-            elif conf['mode'] == 'dot':
-                pass
-            elif conf['mode'] == 'json':
-                pass
+        elif conf['mode'] == 'dot':
+            pass
+        elif conf['mode'] == 'json':
+            pass
     connection.commit()
 
 def read_from_csv(path):
